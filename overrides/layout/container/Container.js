8b795df6b59895819d5f1ceb2ba548e310633ae1
Martin Zeitler <syslogic23@gmail.com>
2017-03-06 11:43:41 +0100

    framework override added, which prevents the "[W] targetCls is missing. This may mean that getTargetEl() is being overridden but not applyTargetCls()." complaint.

---
+++ overrides/layout/container/Container.js
@@ -0,0 +1,14 @@
+/**
+ * Circumvents the framework warning: "[W] targetCls is missing.
+ * This may mean that getTargetEl() is being overridden but not applyTargetCls()."
+ * @class NVD3Charts.overrides.layout.container.Container
+ * @overrides Ext.layout.container.Container
+ * @copyright Copyright 2017 by Martin Zeitler, All rights reserved.
+ * @author https://plus.google.com/+MartinZeitler
+**/
+Ext.define('NVD3Charts.overrides.layout.container.Container', {
+  override: 'Ext.layout.container.Container',
+  notifyOwner: function() {
+    this.owner.afterLayout(this);
+  }
+});
