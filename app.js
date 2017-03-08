7eb6bd9ec413cde7e0f67acb5212a2c3090ac32a
Martin Zeitler <syslogic23@gmail.com>
2017-03-08 10:52:03 +0100

    some code cleanup.

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 /**
- * NVD3 for Sencha ExtJS - Sample Application.
+ * NVD3.js Bindings for Sencha ExtJS
  * @copyright Copyright 2017 by Martin Zeitler, All rights reserved.
  * @author https://plus.google.com/+MartinZeitler
  * @see https://d3js.org & https://nvd3.org
 **/
 
-Ext.Loader.setConfig({disableCaching: false,  scriptCharset: 'utf8', enabled: true, paths: {'Ext': 'ext/src', 'NVD3': 'packages/local/nvd3-charts'}});
-Ext.application({extend: 'NVD3Charts.Application', name: 'NVD3Charts'});
+Ext.Loader.setConfig({
+    disableCaching: false,
+    scriptCharset: 'utf8',
+    enabled: true,
+    paths: {
+        'Ext': 'ext/src',
+        'NVD3': 'packages/local/nvd3-charts'
+    }
+});
+
+Ext.application({
+    extend: 'NVD3Charts.Application',
+    name: 'NVD3Charts'
+});
