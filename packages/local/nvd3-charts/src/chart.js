15e058f2d6d4c0637704a4f48f3ce3705fac2620
Martin Zeitler <syslogic23@gmail.com>
2017-03-08 19:09:39 +0100

    name-space change to NVD3.chart

--- packages/local/nvd3-charts/src/chart.js
+++ packages/local/nvd3-charts/src/chart.js
@@ -7,24 +7,24 @@
 
 /* global Ext */
 
-Ext.define('NVD3.Charts', {
+Ext.define('NVD3.chart', {
     requires: [
         'NVD3.chart.BoxPlotChart',
         'NVD3.chart.BulletChart',
-        'NVD3.chart.DiscreteBarChart',
         'NVD3.chart.CandleStickBarChart',
         'NVD3.chart.CumulativeLineChart',
-        'NVD3.chart.MultiBarHorizontalChart',
+        'NVD3.chart.DiscreteBarChart',
         'NVD3.chart.HistoricalBarChart',
-        'NVD3.chart.SunburstChart',
         'NVD3.chart.LineChart',
         'NVD3.chart.LinePlusBarChart',
         'NVD3.chart.LineWithFocusChart',
+        'NVD3.chart.MultiBarChart',
+        'NVD3.chart.MultiBarHorizontalChart',
         'NVD3.chart.PieChart',
         'NVD3.chart.ScatterChart',
         'NVD3.chart.SparklinePlus',
         'NVD3.chart.StackedAreaChart',
-        'NVD3.chart.MultiBarChart'
+        'NVD3.chart.SunburstChart'
     ],
     constructor: function(config) {
         this.initConfig(config);
