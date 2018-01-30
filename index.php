021a9519656d8dcf266aa9acf93b7a57ef98408d
Martin Zeitler <syslogic23@gmail.com>
2018-01-30 16:32:00 +0100

    index updated.

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE HTML>
 <html manifest="">
 <head>
+    <meta charset="UTF-8">
     <title>NVD3 Bindings for Sencha ExtJS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="author" content="Martin Zeitler, IT Solution Development"/>
     <meta name="copyright" content="Copyright 2017 by Martin Zeitler."/>
-    <link rel="icon" type="image/x-icon" href="https://store.syslogic.io/catalog/view/theme/randomized/image/favicon.ico"/>
+    <link rel="icon" type="image/x-icon" href="https://store.syslogic.io/catalog/view/theme/syslogic/image/favicon.ico"/>
     <link rel="publisher" href="https://plus.google.com/113310724040129613521"/>
     <link rel="author" href="https://plus.google.com/106963082057954766426"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="resources/themes/ext-theme-crisp/ext-theme-crisp-all.css"/>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.5/nv.d3.min.css"/>
     <link rel="stylesheet" type="text/css" href="resources/css/styles.css"/>
