dcf91b6f39296f32f75f77643e9fde7f3ad3bb7f
Martin Zeitler <syslogic23@gmail.com>
2018-01-31 15:00:28 +0100

    historical bar chart added.

--- app/view/main/Northern.js
+++ app/view/main/Northern.js
@@ -2,7 +2,6 @@
  * NVD3.js Bindings for Sencha ExtJS
  * @copyright Copyright 2017-2018 by Martin Zeitler, Bavaria.
  * @author https://plus.google.com/106963082057954766426
- * @see https://d3js.org & https://nvd3.org
 **/
 
 Ext.define('NVD3Charts.view.main.Northern', {
