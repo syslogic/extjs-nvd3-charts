2dbb56d1f51c32c7f8654520d1fc1a1f01876d09
Martin Zeitler <syslogic23@gmail.com>
2018-02-01 10:13:33 +0100

    northern toolbar updated.

--- app/view/main/Northern.js
+++ app/view/main/Northern.js
@@ -14,10 +14,20 @@ Ext.define('NVD3Charts.view.main.Northern', {
     height: 40,
     border: 0,
     style: {borderBottom: '1px solid #157FCC !important', background: '#F5F5F5', color: '#157FCC'},
-    items: [
-        {xtype: 'label', text: 'NVD3.js Bindings for Sencha ExtJS', padding: 8}, '->',
-        {xtype: 'label', html: '<a class="github" href="https://github.com/syslogic/extjs-nvd3-charts" target="_blank"><img src="resources/images/octocat.png"></a>', paddingTop: 9, width: 22},
-        {xtype: 'label', html: '<a class="gplus" href="https://plus.google.com/106963082057954766426?rel=author" target="_blank"><img src="resources/images/gplus.png"></a>', paddingTop: 6, width: 20},
-        {xtype: 'label', html: '<a class="paypal" href="https://www.paypal.me/syslogic" target="_blank"><img src="resources/images/paypal.png"></a>', paddingTop: 9, width: 20}
-    ]
+    defaults: {xtype: 'label'},
+    items: [{
+            text: 'NVD3.js for Sencha ExtJS',
+            padding: 8
+    }, '->', {
+        html: '<a class="github" href="https://github.com/syslogic/extjs-nvd3-charts" target="_blank"><img src="resources/images/octocat.png"></a>',
+        paddingTop: 10,
+        width: 22
+    }, {
+        html: '<a class="gplus" href="https://plus.google.com/106963082057954766426?rel=author" target="_blank"><img src="resources/images/gplus.png"></a>',
+        paddingTop: 5, width: 20
+    }, {
+        html: '<a class="paypal" href="https://www.paypal.me/syslogic" target="_blank"><img src="resources/images/paypal.png"></a>',
+        paddingTop: 9,
+        width: 20
+    }]
 });
