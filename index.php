e3717833793b7b5d4464b579a0adc9fc014c0d9d
Martin Zeitler <syslogic23@gmail.com>
2018-02-01 10:08:14 +0100

    using a patched version of the library, too.

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 <html manifest="">
 <head>
     <meta charset="UTF-8">
-    <title>NVD3 Bindings for Sencha ExtJS</title>
+    <title>NVD3 for Sencha ExtJS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="author" content="Martin Zeitler, IT Solution Development"/>
-    <meta name="copyright" content="Copyright 2017 by Martin Zeitler."/>
+    <meta name="copyright" content="Copyright 2017-2018 by Martin Zeitler, Bavaria."/>
     <link rel="icon" type="image/x-icon" href="https://store.syslogic.io/catalog/view/theme/syslogic/image/favicon.ico"/>
     <link rel="publisher" href="https://plus.google.com/113310724040129613521"/>
     <link rel="author" href="https://plus.google.com/106963082057954766426"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" type="text/css" href="resources/themes/ext-theme-crisp/ext-theme-crisp-all.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.5/nv.d3.min.css"/>
+    <link rel="stylesheet" type="text/css" href="lib/nvd3/nv.d3.min.css"/>
     <link rel="stylesheet" type="text/css" href="resources/css/styles.css"/>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.2/d3.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.5/nv.d3.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js"></script>
+    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/nvd3/1.8.6/nv.d3.min.js"></script> -->
+    <script type="text/javascript" src="lib/nvd3/nv.d3.js"></script>
     <script type="text/javascript" src="application.js"></script>
 </head>
 <body></body>
